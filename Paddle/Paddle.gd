913351726ae0882e345f024aea822ab4c7fa236b
Pyshrub <thepyromancer2048@gmail.com>
2023-10-08 16:13:38 -0400

    kkk
    working

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -28,3 +28,6 @@ func hit(_ball):
 	tween.tween_property($Images/Highlight, "modulate:a", 0.0, time_highlight)
 	$Images/Highlight.scale = Vector2(2.0,2.0)
 	tween.tween_property($Images/Highlight, "scale", Vector2(1.0,1.0), time_highlight_size).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
+	var pad_sound = get_node_or_null("/root/Game/Ball_Sound")
+	if pad_sound != null:
+		pad_sound.play()
